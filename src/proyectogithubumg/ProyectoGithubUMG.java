520802f4bf7ce905efa3a8796992e97399bcdd0f
UMG <UMG@JOC-LAB-1H2.labjoc.local>
2026-04-14 18:40:03 -0600

    Mi primera version del codigo

---
+++ src/proyectogithubumg/ProyectoGithubUMG.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyectogithubumg;
+
+/**
+ *
+ * @author UMG
+ */
+public class ProyectoGithubUMG {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        System.out.println("hola papus");
+        System.out.println(":)");
+    }
+    
+}
